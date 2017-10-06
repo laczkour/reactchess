1d94ae70183d3e0e8141731aff05ff19ef35fbeb
LaczkoUr <laczkour@gmail.com>
2017-10-06 16:20:41 +0200

    followed through readme.md expect jest

---
+++ src/containers/Hello.tsx
@@ -0,0 +1,20 @@
+import Hello from '../components/Hello';
+import * as actions from '../actions/index';
+import { StoreState } from '../types/index';
+import { connect, Dispatch } from 'react-redux';
+
+export function mapStateToProps({ enthusiasmLevel, languageName }: StoreState) {
+  return {
+    enthusiasmLevel,
+    name: languageName,
+  };
+}
+
+export function mapDispatchToProps(dispatch: Dispatch<actions.EnthusiasmAction>) {
+  return {
+    onIncrement: () => dispatch(actions.IncrementEnthusiasm()),
+    onDecrement: () => dispatch(actions.DecrementEnthusiasm()),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Hello);
